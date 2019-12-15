b08510e5457065a9fa5d38b4daab238616abdcd0
VKen <vken85@gmail.com>
2019-12-15 15:58:57 +0800

    added POST API for new threads to board

--- routes/api.js
+++ routes/api.js
@@ -9,11 +9,41 @@
 'use strict';
 
 var expect = require('chai').expect;
+var ObjectId = require('mongodb').ObjectID;
 
-module.exports = function (app) {
-  
-  app.route('/api/threads/:board');
-    
+module.exports = function (app, client) {
+
+  app.route('/api/threads/:board')
+    .post(async (req, res) => {
+        const board = req.params.board
+        const col = client.db().collection(board);
+        const required_fields = ['text', 'delete_password'];
+        let missing = [];
+        if (!required_fields.every((ele, idx) => {
+            if (req.body[ele] === undefined) {
+                missing.push(ele);
+                return false;
+            }
+            return true
+        })) {
+            return res.status(422).send(`missing required fields ${JSON.stringify(missing)}`);
+        }
+
+        let now = new Date();
+        let r = await col.insertOne({
+            text: req.body.text,
+            delete_password: req.body.delete_password,  // TODO: hash?
+            created_on: now,
+            bumped_on: now,
+            reported: false,
+        });
+        if (r.insertedCount == 1) {
+            return res.redirect(`/b/${board}`);
+        }
+        return res.status(500).send('Database error.');
+
+    });
+
   app.route('/api/replies/:board');
 
 };
--- server.js
+++ server.js
@@ -5,6 +5,10 @@ var bodyParser  = require('body-parser');
 var expect      = require('chai').expect;
 var cors        = require('cors');
 var helmet        = require('helmet');
+var MongoClient = require('mongodb').MongoClient;
+var ObjectId = require('mongodb').ObjectID;
+const CONNECTION_STRING = process.env.DB;
+const mongo = new MongoClient(CONNECTION_STRING, { useUnifiedTopology: true });
 
 var apiRoutes         = require('./routes/api.js');
 var fccTestingRoutes  = require('./routes/fcctesting.js');
@@ -21,53 +25,62 @@ app.use(cors({origin: '*'})); //For FCC testing purposes only
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-//Sample front-end
-app.route('/b/:board/')
-  .get(function (req, res) {
-    res.sendFile(process.cwd() + '/views/board.html');
-  });
-app.route('/b/:board/:threadid')
-  .get(function (req, res) {
-    res.sendFile(process.cwd() + '/views/thread.html');
-  });
+mongo.connect((err, client) => {
+    if (err) {
+        console.error('connection error to db');
+    } else {
+        console.log('db connection successful');
+        //Sample front-end
+        app.route('/b/:board/')
+        .get(function (req, res) {
+            res.sendFile(process.cwd() + '/views/board.html');
+        });
+        app.route('/b/:board/:threadid')
+        .get(function (req, res) {
+            res.sendFile(process.cwd() + '/views/thread.html');
+        });
 
-//Index page (static HTML)
-app.route('/')
-  .get(function (req, res) {
-    res.sendFile(process.cwd() + '/views/index.html');
-  });
+        //Index page (static HTML)
+        app.route('/')
+        .get(function (req, res) {
+            res.sendFile(process.cwd() + '/views/index.html');
+        });
 
-//For FCC testing purposes
-fccTestingRoutes(app);
+        //For FCC testing purposes
+        fccTestingRoutes(app);
 
-//Routing for API
-apiRoutes(app);
+        //Routing for API
+        apiRoutes(app, client);
 
-//Sample Front-end
+        //Sample Front-end
 
 
-//404 Not Found Middleware
-app.use(function(req, res, next) {
-  res.status(404)
-    .type('text')
-    .send('Not Found');
-});
+        //404 Not Found Middleware
+        app.use(function(req, res, next) {
+        res.status(404)
+            .type('text')
+            .send('Not Found');
+        });
 
-//Start our server and tests!
-app.listen(process.env.PORT || 3000, function () {
-  console.log("Listening on port " + process.env.PORT);
-  if(process.env.NODE_ENV==='test') {
-    console.log('Running Tests...');
-    setTimeout(function () {
-      try {
-        runner.run();
-      } catch(e) {
-        var error = e;
-          console.log('Tests are not valid:');
-          console.log(error);
-      }
-    }, 1500);
-  }
+        //Start our server and tests!
+        //
+        const port = process.env.PORT || 3000;
+        app.listen(port, function () {
+            console.log("Listening on port " + port);
+            if(process.env.NODE_ENV==='test') {
+                console.log('Running Tests...');
+                setTimeout(function () {
+                    try {
+                        runner.run();
+                    } catch(e) {
+                        var error = e;
+                        console.log('Tests are not valid:');
+                        console.log(error);
+                    }
+                }, 1500);
+            }
+        });
+    }
 });
 
 module.exports = app; //for testing
